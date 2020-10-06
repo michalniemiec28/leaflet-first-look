00a4f27558415acbbad150126f8011b11775f2f6
Michał Niemiec <michal.niemiec@billennium.com>
2020-10-06 17:15:14 +0200

    Updates

---
+++ precache-manifest.45e04fd4af9680b7c74d8064be5af693.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b3778b4137824889383b2daf311759e",
+    "url": "/leaflet-first-look/index.html"
+  },
+  {
+    "revision": "7270049adec2fc0d8864",
+    "url": "/leaflet-first-look/static/css/2.a4567992.chunk.css"
+  },
+  {
+    "revision": "bfe97b0c346455a4b804",
+    "url": "/leaflet-first-look/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "7270049adec2fc0d8864",
+    "url": "/leaflet-first-look/static/js/2.958a5f9a.chunk.js"
+  },
+  {
+    "revision": "0580f66cff7a7b2e7b5c118fd842bcfd",
+    "url": "/leaflet-first-look/static/js/2.958a5f9a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bfe97b0c346455a4b804",
+    "url": "/leaflet-first-look/static/js/main.0340671b.chunk.js"
+  },
+  {
+    "revision": "5ceee15758c97ea214af",
+    "url": "/leaflet-first-look/static/js/runtime-main.3e85aff7.js"
+  }
+]);
